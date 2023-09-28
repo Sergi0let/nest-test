d8226bd09245ce517875c443cb05f5a23ddc4180
Sergij Vashkevych <88962825+Sergi0let@users.noreply.github.com>
2023-09-28 23:31:14 +0300

    add controllers

--- src/entities/user/user.controller.ts
+++ src/entities/user/user.controller.ts
@@ -8,35 +8,55 @@ import {
   Req,
   Res,
   UseInterceptors,
+  Param,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 import { UserService } from './user.service';
 
-@Controller('users')
+@Controller('users') // /users/ - це початок роуту
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('/')
+  @Get('/') // /users/
   async getAllUsers(@Req() req: Request, @Res() res: Response) {}
 
-  @Get('/:id')
-  async getUser(@Req() req: Request, @Res() res: Response) {}
+  @Get('/:id') // /users/:id
+  async getUser(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ) {
+    const userData = await this.userService.getUserData(id);
+    const { email, nameFirst, nameLast, birthDate, gender } = userData;
+    return res.send({
+      status: 'ok',
+      data: { email, nameFirst, nameLast, birthDate, gender },
+    });
+  }
 
-  @Post('/')
+  @Post('/') // /users/
   @UseInterceptors(FileInterceptor(''))
   async addUser(@Req() req: Request, @Res() res: Response) {
     await this.userService.addUser(req.body);
     return res.send({ status: 'ok' });
   }
 
-  @Put('/:id')
+  @Put('/:id') // /users/:id
   async updateUser(@Req() req: Request, @Res() res: Response) {}
 
-  @Patch('/:id')
+  @Patch('/:id') // /users/:id
   async updateUserField(@Req() req: Request, @Res() res: Response) {}
 
-  @Delete('/:id')
-  async deleteUser(@Req() req: Request, @Res() res: Response) {}
+  @Delete('/:id') // /users/:id
+  async deleteUser(
+    @Req() req: Request,
+    @Param('id', ParseIntPipe) id: number,
+    @Res() res: Response,
+  ) {
+    await this.userService.deleteUser(id);
+    return res.send({ status: 'ok' });
+  }
 }
--- src/entities/user/user.service.ts
+++ src/entities/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { genSalt, hash } from 'bcrypt';
 
 import { User } from './user.entity';
 
@@ -10,9 +11,24 @@ export class UserService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  //Registery new user
+  //! Registery new user
   public async addUser(userData: any) {
-    const newUser = this.userRepository.create(userData);
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(userData.password, salt);
+    const newUser = this.userRepository.create({
+      ...userData,
+      password: hashedPassword,
+    });
     await this.userRepository.save(newUser);
+  }
+
+  //! Get user data by id
+  public async getUserData(id: number) {
+    return await this.userRepository.findOne({ where: { id } });
+  }
+
+  //! Delete user by id
+  public async deleteUser(id: number) {
+    return await this.userRepository.delete(id);
   }
 }
